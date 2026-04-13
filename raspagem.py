95ad71027c71ab53503de8e75aff69a01444da21
Denival Biotto Filho <denivaldenival@gmail.com>
2026-04-13 15:45:37 -0300

    Update raspagem.py

--- raspagem.py
+++ raspagem.py
@@ -8,3 +8,9 @@ url = "https://www.ifsp.edu.br/index.php/ultimas-noticias"
 resposta = requests.get(url)
 
 sopa = BeautifulSoup(resposta.text, 'html.parser')
+
+noticias = sopa.find_all('h2', class_='tile-headline') 
+# Porque esse h2? No HTML, os textos são organizados por hierarquia de importância, indo do 1 ao 6:
+#    <h1>: O título principal da página (geralmente o nome do site ou o assunto principal).
+#    <h2>: Subtítulos ou títulos de seções (como os títulos de cada notícia).
+#    <h3> a <h6>: Títulos menores, para subseções dentro dos temas.
